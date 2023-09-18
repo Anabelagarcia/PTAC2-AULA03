2a1702027c02f18c49367a0ee9b35fa724881990
Anabela <anabelanams@gmail.com>
2023-09-18 10:57:22 -0400

    TRABALHOPTAS1

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -2,18 +2,25 @@ import { useState } from "react";
 
 import { Link } from "react-router-dom";
 
-export default function ToDo() {
+export default function Todo() {
 
     const [atividade ,setAtividade] = useState("");
     const [lista , setLista] = useState([]);
-    const [id , setId] = useState(Math.floor(Math.randoom(0,100)));
+    const [id , setId] = useState(Math.floor(Math.random(0,100)));
+    const [preco , setPreco] = useState(0);
 
     const salvar = (e) => {
         e.preventDefault();
         setLista([...lista, {
-            atividade: atividade
-        id:id
+            atividade: atividade,
+            id:id,
+            preco:preco
+
         }]);
+
+        setId(id+1);
+        setAtividade("");
+        setPreco("");
     };
 
     function remover (id) {
@@ -26,22 +33,33 @@ export default function ToDo() {
 
     return (
      <div>
-        <h1>todo</h1>
         <Link to="/">HOME</Link>
+        <h1>ADD</h1>
+        
         <form onSubmit={salvar}>
             <input
               type="text"
               value={atividade}
               onChange={(e) => { setAtividade (e.target.value);
             }}/>
+            <input
+              type="number"
+              value={preco}
+              onChange={(e) => { setPreco (e.target.value);
+            }}/>
+
+
 
         <button>ADD</button>
         </form>
         {lista.map((atividade)=> ( <div key={atividade.id0}>
             <p>{atividade.atividade}</p>
+            <p>{atividade.preco}</p>
             <button onClick={(e)=> e.preventDefault(remover(atividade.id))}>remover</button>
             </div>
             ))}
             </div>
+
+            
         );
     }
